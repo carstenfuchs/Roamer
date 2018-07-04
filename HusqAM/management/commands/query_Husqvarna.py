11c0c02eb9f306277b97360ca61ce229f7d3435a
Carsten Fuchs <carsten.fuchs@cafu.de>
2018-07-04 11:36:59 +0200

    Teach management comment `query_Husqvarna` to update the `Robot` instances in the database.

--- HusqAM/management/commands/query_Husqvarna.py
+++ HusqAM/management/commands/query_Husqvarna.py
@@ -1,14 +1,16 @@
+from datetime import datetime, timedelta
+import json, pyhusmow
 from django.conf import settings
 from django.core.management.base import BaseCommand
-import pprint, pyhusmow
-from datetime import datetime, timedelta
+from HusqAM.models import Robot
 
 
 class Command(BaseCommand):
-    help = "Ruft den logischen Zustand und die aktuellen Betriebsdaten der Mäher von www.husquarna.com ab und aktualisiert damit (--update-database) die Datenbank."
+    help = "Ruft den logischen Zustand und die aktuellen Betriebsdaten der Mäher von www.husquarna.com ab."
 
     def add_arguments(self, parser):
-        parser.add_argument('-u', '--update-database', action='store_true', help='Aktualisiere die Datenbank mit den abgerufenen Informationen.')
+        parser.add_argument('-u', '--update-database', action='store_true', help='Aktualisiert die lokale Datenbank mit den abgerufenen Informationen.')
+        parser.add_argument('-p', '--post-to-remote', metavar='SERVER', help='Sendet die abgerufenen Informationen per POST Request an den entfernten Server.')
 
     def handle(self, *args, **options):
         # Das Token ist wie ein Cookie: Daran erkennt der Husquarna-Server uns wieder.
@@ -26,12 +28,22 @@ class Command(BaseCommand):
             tc.save_config()
             self.stdout.write('Created a new token.')
 
-        for robot in mow.list_robots():
-            s = pprint.pformat(robot, indent=4)
-            self.stdout.write(s)
+        for robot_dict in mow.list_robots():
+            self.stdout.write(json.dumps(robot_dict, indent=4))
             self.stdout.write("\n")
 
             # mow.select_robot(robot['id'])
             # s = pprint.pformat(mow.status(), indent=4)
             # self.stdout.write(s)
             # self.stdout.write("\n")
+
+        if options['update_database']:
+            for robot_dict in mow.list_robots():
+                robot = Robot.objects.get(manufac_id=robot_dict['id'])
+                changes = robot.update_from_dict(robot_dict)
+                robot.save()
+                self.stdout.write(str(robot) + " – ", ending="")
+                self.stdout.write(", ".join(changes) if changes else "keine Änderungen")
+
+        if options['post_to_remote']:
+            pass
