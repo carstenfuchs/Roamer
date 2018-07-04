c55fcb818b038de1a40f393a126ed95fa121c80a
Carsten Fuchs <carsten.fuchs@cafu.de>
2018-07-04 11:33:38 +0200

    Make `Robot` accessible in the Django Admin

--- HusqAM/admin.py
+++ HusqAM/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Robot
 
-# Register your models here.
+
+class RobotAdmin(admin.ModelAdmin):
+	list_display = ('owner', 'manufac_id', 'manufac_model', 'given_name')
+
+admin.site.register(Robot, RobotAdmin)
