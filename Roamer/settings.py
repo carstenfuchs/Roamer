cfd434c900fa67167d0e75c455aaad2b06dea3cc
Carsten Fuchs <carsten.fuchs@cafu.de>
2018-07-07 14:28:16 +0200

    Add static resources/files for use by the generated websites.

--- Roamer/settings.py
+++ Roamer/settings.py
@@ -122,4 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = '/var/www/Roamer-static/'
+
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
